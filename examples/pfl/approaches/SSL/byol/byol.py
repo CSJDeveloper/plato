10a8cf1b0d033056d370fd84eca8cba0ed3b2596
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-05-20 21:54:35 -0400

    Adjusted the code to import ssl model from pflbases

--- examples/pfl/approaches/SSL/byol/byol.py
+++ examples/pfl/approaches/SSL/byol/byol.py
@@ -1,5 +1,5 @@
 """
-The implementation for the BYOL [1] method.
+Implementation of the BYOL [1] method.
 
 [1]. Jean-Bastien Grill, et.al, Bootstrap Your Own Latent A New Approach to Self-Supervised Learning.
 https://arxiv.org/pdf/2006.07733.pdf.
@@ -7,16 +7,11 @@ https://arxiv.org/pdf/2006.07733.pdf.
 Source code: https://github.com/lucidrains/byol-pytorch
 The third-party code: https://github.com/sthalles/PyTorch-BYOL
 """
-import copy
-
-from torch import nn
-from lightly.models.modules import BYOLPredictionHead, BYOLProjectionHead
-from lightly.models.utils import deactivate_requires_grad, update_momentum
-from lightly.utils.scheduler import cosine_schedule
 
 from plato.trainers import loss_criterion
-from plato.models.cnn_encoder import Model as encoder_registry
-from plato.config import Config
+
+from lightly.models.utils import update_momentum
+from lightly.utils.scheduler import cosine_schedule
 
 from pflbases import fedavg_personalized_server
 from pflbases import fedavg_partial
@@ -24,6 +19,7 @@ from pflbases import fedavg_partial
 from pflbases.trainer_callbacks import separate_trainer_callbacks
 from pflbases.trainer_callbacks import ssl_trainer_callbacks
 from pflbases.client_callbacks import local_completion_callbacks
+from pflbases.models import byol
 
 from pflbases import ssl_client
 from pflbases import ssl_trainer
@@ -76,61 +72,6 @@ class Trainer(ssl_trainer.Trainer):
             )
 
 
-class BYOL(nn.Module):
-    def __init__(self, encoder=None, encoder_dim=None):
-        super().__init__()
-
-        # define the encoder
-        encoder_name = Config().trainer.encoder_name
-        encoder_params = (
-            Config().params.encoder if hasattr(Config().params, "encoder") else {}
-        )
-
-        # define the encoder
-        self.encoder = (
-            encoder
-            if encoder is not None
-            else encoder_registry.get(model_name=encoder_name, **encoder_params)
-        )
-
-        self.encoding_dim = self.encoder.encoding_dim
-        projection_hidden_dim = Config().trainer.projection_hidden_dim
-        projection_out_dim = Config().trainer.projection_out_dim
-        prediction_hidden_dim = Config().trainer.prediction_hidden_dim
-        prediction_out_dim = Config().trainer.prediction_out_dim
-
-        self.projection_head = BYOLProjectionHead(
-            self.encoding_dim, projection_hidden_dim, projection_out_dim
-        )
-        self.prediction_head = BYOLPredictionHead(
-            projection_out_dim, prediction_hidden_dim, prediction_out_dim
-        )
-
-        self.encoder_momentum = copy.deepcopy(self.encoder)
-        self.projection_head_momentum = copy.deepcopy(self.projection_head)
-
-        deactivate_requires_grad(self.encoder_momentum)
-        deactivate_requires_grad(self.projection_head_momentum)
-
-    def forward_direct(self, samples):
-        encoded_examples = self.encoder(samples).flatten(start_dim=1)
-        projected_examples = self.projection_head(encoded_examples)
-        output = self.prediction_head(projected_examples)
-        return output
-
-    def forward_momentum(self, samples):
-        encoded_examples = self.encoder_momentum(samples).flatten(start_dim=1)
-        projected_examples = self.projection_head_momentum(encoded_examples)
-        projected_examples = projected_examples.detach()
-        return projected_examples
-
-    def forward(self, multiview_samples):
-        samples1, samples2 = multiview_samples
-        output1 = self.forward_direct(samples1)
-        projected_samples1 = self.forward_momentum(samples1)
-        output2 = self.forward_direct(samples2)
-        projected_samples2 = self.forward_momentum(samples2)
-        return (output1, projected_samples2), (output2, projected_samples1)
 
 
 def main():
@@ -139,7 +80,7 @@ def main():
     """
     trainer = Trainer
     client = ssl_client.Client(
-        model=BYOL,
+        model=byol.BYOL,
         datasource=ssl_datasources.TransformedDataSource,
         personalized_datasource=ssl_datasources.TransformedDataSource,
         trainer=trainer,
@@ -154,7 +95,7 @@ def main():
         ],
     )
     server = fedavg_personalized_server.Server(
-        model=BYOL,
+        model=byol.BYOL,
         trainer=trainer,
         algorithm=fedavg_partial.Algorithm,
     )
