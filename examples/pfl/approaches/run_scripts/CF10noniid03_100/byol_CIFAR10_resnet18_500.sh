b8667f57f0fce5dfc9a50b6367ca888d8cd387e8
Sijia Chen <CSJDeveloper@users.noreply.github.com>
2023-06-04 19:01:24 -0400

    Revised the slurm's sbatch running file

--- examples/pfl/approaches/run_scripts/CF10noniid03_100/byol_CIFAR10_resnet18_500.sh
+++ examples/pfl/approaches/run_scripts/CF10noniid03_100/byol_CIFAR10_resnet18_500.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
-#SBATCH --time=200:00:00 
+#SBATCH --time=24:00:00 
 #SBATCH --cpus-per-task=12 
 #SBATCH --gres=gpu:1 
 #SBATCH --mem=72G 
 #SBATCH --output=/Users/sjia/Documents/Research/MyPapers/contrastivePFL/repo/code/plato/examples/pfl/slurm_loggings/CF10noniid03_100/byol_CIFAR10_resnet18_500.out 
 
-/home/sijia/envs/miniconda3/envs/INFOCOM23/bin/python /Users/sjia/Documents/Research/MyPapers/contrastivePFL/repo/code/plato/examples/pfl/approaches/SSL/byol/byol.py -c /Users/sjia/Documents/Research/MyPapers/contrastivePFL/repo/code/plato/examples/pfl/approaches/configs/CF10noniid03_100/byol_CIFAR10_resnet18_500.yml -b /data/sijia/NIPS23/experiments/CF10noniid03_100
+/home/sijia/envs/miniconda3/envs/INFOCOM23/bin/python /Users/sjia/Documents/Research/MyPapers/contrastivePFL/repo/code/plato/examples/pfl/approaches/SSL/byol/byol.py -c /Users/sjia/Documents/Research/MyPapers/contrastivePFL/repo/code/plato/examples/pfl/approaches/configs/CF10noniid03_100/byol_CIFAR10_resnet18_500.yml -b /data/sijia/NIPS23/experiments/CF10noniid03_100/byol_CIFAR10_resnet18_500
